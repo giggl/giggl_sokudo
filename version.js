9798bb513c8e5354a36ecc95769de0955ad6c63a
Rob Morrissey <me@rmorrissey.io>
2021-08-27 19:21:52 +0100

    use cjs

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-import {readFileSync, writeFileSync} from 'fs';
+const {readFileSync, writeFileSync} = require('fs');
 
 const [releaseVersion] = process.argv.slice(2);
 const packageData = JSON.parse(readFileSync('./package.json', 'utf-8'));
