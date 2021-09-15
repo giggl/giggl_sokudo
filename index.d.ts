b384acb4f4706914398756f7d0702a8bb5f6ff25
Rob Morrissey <me@rmorrissey.io>
2021-09-15 05:53:30 +0100

    more types

--- index.d.ts
+++ index.d.ts
@@ -1,52 +1,55 @@
-enum MethodType {
-    NODE_BUFFER ,
-    MSGPACK 
+declare enum MethodType {
+  NODE_BUFFER,
+  MSGPACK,
 }
+
 declare interface Methods {
-    NODE_BUFFER:number = 0,
-    MSGPACK:number = 1,
+  NODE_BUFFER: 0;
+  MSGPACK: 1;
 }
 
 declare interface Handler {
-    op: number;
-    eventName: string;
-    packer: (data: any, method: MethodType) => Buffer;
-    unpacker: (buffer: Buffer, method: MethodType) => any;
-
+  op: number;
+  eventName: string;
+  packer: <T>(data: T, method: MethodType) => Buffer;
+  unpacker: (buffer: Buffer, method: MethodType) => any;
 }
 
 declare class Client {
-    send(opCode: number, data: any): void;
-
+  send(opCode: number, data: any): void;
+  close(): void;
 }
 
 declare class Connection {
-    useHandler(handler: Handler): void;
-    unregisterHandler(name: number | string);
-    send(opCode: number, data: any): void;
-    close(): void;
-    connect(): void;
-
+  useHandler(handler: Handler): void;
+  unregisterHandler(name: number | string);
+  send(opCode: number, data: any): void;
+  close(): void;
+  connect(): void;
 }
 
 declare class Server {
-    listen(port: number | string, bindAddress?: string): Promise<void | Error>;
-    useHandler(handler: Handler): void;
-    unregisterHandler(name: number | string);
+  listen(port: number | string, bindAddress?: string): Promise<void | Error>;
+  useHandler(handler: Handler): void;
+  unregisterHandler(name: number | string);
 }
-
 
 interface ServerProps {
-    version?: number = 1;
-    methods: Methods;
+  version?: number;
+  methods: Methods;
 }
+
 interface ClientProps {
-    autoReconnect?: boolean;
-    heartbeatInterval?: number;
-    replay?: boolean;
+  autoReconnect?: boolean;
+  heartbeatInterval?: number;
+  replay?: boolean;
 }
 
 declare namespace Sokudo {
-    declare function Client(host: string, port: string | number, props?: ClientProps): Connection;
-    declare function Server(props?: ServerProps): Server;
+  function Client(
+    host: string,
+    port: string | number,
+    props?: ClientProps
+  ): Connection;
+  function Server(props?: ServerProps): Server;
 }
